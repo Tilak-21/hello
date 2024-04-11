a67e8154947c9cfcf962408f80bf802d6f30b295
Tilak-21 <tilakamlesh2112@gmail.com>
2024-04-11 05:05:47 -0400

    added new route to views page

--- index.php
+++ index.php
@@ -15,7 +15,11 @@ $f3 = Base::instance();
 
 //Define a default route
 $f3->route('GET /', function() {
-    echo '<h1>Hello Fat-Free</h1>';
+//    echo '<h1>Hello Fat-Free</h1>';
+
+    $view = new Template();
+    echo $view->render('views/home.html');
+
 });
 
 //Run Fat-Free
